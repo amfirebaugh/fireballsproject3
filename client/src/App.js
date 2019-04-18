ab20f8fe84de2a15e79746d5e8ba5d59e79ed180
Phoenicis <alliefirebaugh@gmail.com>
2019-04-17 23:24:04 -0400

    working login and logout on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,62 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 // react router imports
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 // import pages
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Callback from './components/Callback';
+
+// auth imports and components
+import AuthContext from './Auth/AuthContext';
+import Auth from './Auth/Auth';
+import PrivateRoute from './Auth/PrivateRoute';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: new Auth(this.props.history),
+      tokenRenewalComplete: false
+    };
+  }
+
+  componentDidMount() {
+    this.state.auth.renewToken(() => {
+      this.setState({ tokenRenewComplete: true });
+    });
+  }
+
   render() {
+    const { auth } = this.state;
+
     return (
-      <BrowserRouter>
-        <div className="App">
-          {/* Render Navbar */}
-          <Navbar branding="Dummy Navbar" />
-          {/* 
+      <AuthContext.Provider value={auth}>
+        <Router>
+          <div className="App">
+            {/* Render Navbar */}
+            <Navbar branding="Dummy Navbar" auth={auth} />
+            {/* 
             set up the routes, ALL routes need to be imported at top 
             the '/' = Home and 'other routes contain '/', 
             they will be appended to home route in the browser window
             ** However **
             adding exact to '/' fixes this
           */}
-          <Switch>
+            {/* <Redirect
+            path="/callback"
+            render={props => <Callback auth={auth} {...props} />}
+          /> */}
             <Route exact path="/" component={Home} />
-            <Route exact path="/Profile" component={Profile} />
+            <PrivateRoute exact path="/profile" component={Profile} />
             {/* 
-              Profile is a placeholder route until we get auth ironed out 
-              <Route exact path="/Profile" Component={Profile} /> 
-            */}
-          </Switch>
-        </div>
-      </BrowserRouter>
+            Profile is a placeholder route until we get auth ironed out 
+            <Route exact path="/Profile" Component={Profile} /> 
+          */}
+          </div>
+        </Router>
+      </AuthContext.Provider>
     );
   }
 }
