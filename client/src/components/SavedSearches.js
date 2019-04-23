d0f20ef22c6e7132ad2d015f6173d25018aee9d2
thedern <dxsmith@soverin.net>
2019-04-23 10:20:08 -0400

    fixed table non-reponsive, bootstrap text color override for table

--- client/src/components/SavedSearches.js
+++ client/src/components/SavedSearches.js
@@ -5,10 +5,10 @@ export function SavedSearches(props) {
   console.log(drug1, drug2, age, sex);
   return (
     <tr>
-      <td className="p-3">{drug1}</td>
-      <td className="p-3">{drug2}</td>
-      <td className="p-3">{age}</td>
-      <td className="p-3">{sex}</td>
+      <td className="pl-3">{drug1}</td>
+      <td className="pl-3">{drug2}</td>
+      <td className="pl-3">{age}</td>
+      <td className="pl-3">{sex}</td>
     </tr>
   );
 }
