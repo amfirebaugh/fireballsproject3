d47efb2a7efda8dd2963369d0558377960481975
thedern <dxsmith@soverin.net>
2019-04-26 10:59:18 -0400

    found and fixed broken search and resp headers errors

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -71,7 +71,7 @@ router.post('/interaction', function(req, res) {
   //**** PLACE DRUGS IN AN INTERMEDIATE ARRAY AND SORT DRUG NAMES ALPHABETICALLY - SEARCHES SHOULD ALWAYS BE IN SAME ORDER ****//
   let dAlpha = [];
   dAlpha.push(drugnames[0], drugnames[1]);
-  dAlpha.sort();
+  dAlpha.sort().reverse();
 
   //find a matching drug combo in the DrufDetails DB
   db.DrugDetails.find({
@@ -102,7 +102,7 @@ router.post('/interaction', function(req, res) {
           .then(dbUser => {
             console.log('saved to user ', dbUser);
             // run interaction query
-            // interactionQuery();
+            interactionQuery();
           })
           .catch(err => {
             console.log('error saving and associating drug with user', err);
@@ -153,8 +153,11 @@ router.post('/interaction', function(req, res) {
                     { $push: { drugDetails: dbDrugSaved.id } },
                     { new: true }
                   ); // end update
+                })
+                .then(dbUser => {
+                  console.log('saved to user ', dbUser);
                   // run interaction query
-                  // interactionQuery();
+                  interactionQuery();
                 })
                 .catch(err => {
                   console.log(
@@ -165,7 +168,7 @@ router.post('/interaction', function(req, res) {
             } else {
               console.log('user already has this combo recorded');
               // run interaction query
-              // interactionQuery();
+              interactionQuery();
             } // end if for matchCount
           }); // end promise for drug pre-existing in db
       } // end outer else
@@ -174,92 +177,99 @@ router.post('/interaction', function(req, res) {
       console.log('error finding drug associated with user', err);
     }); // end find user for drug combo entered
 
-  //function interactionQuery() {
-  // run API for interaction
-  var queryUrl =
-    'https://www.ehealthme.com/api/v1/drug-interaction/' +
-    dAlpha[0] +
-    '/' +
-    dAlpha[1] +
-    '/';
+  function interactionQuery() {
+    // run API for interaction
+    var queryUrl =
+      'https://www.ehealthme.com/api/v1/drug-interaction/' +
+      dAlpha[0] +
+      '/' +
+      dAlpha[1] +
+      '/';
 
-  axios
-    .get(queryUrl)
-    .then(function(response) {
-      // TEST SHORTCUT to send data to browser console before try loop
-      // res.json(response.data);
+    axios
+      .get(queryUrl)
+      .then(function(response) {
+        // TEST SHORTCUT to send data to browser console before try loop
+        // res.json(response.data);
 
-      try {
-        test = response;
-        for (var i = 0; i < test.data.age_interaction[age].length; i++) {
-          for (
-            var j = 0;
-            j < test.data.gender_interaction[gender].length;
-            j++
-          ) {
-            if (
-              test.data.age_interaction[age][i] ===
-              test.data.gender_interaction[gender][j]
+        try {
+          test = response;
+          for (var i = 0; i < test.data.age_interaction[age].length; i++) {
+            for (
+              var j = 0;
+              j < test.data.gender_interaction[gender].length;
+              j++
             ) {
-              mostLikelySymptoms += test.data.age_interaction[age][i] + ' || ';
-            }
-          }
-        }
-        console.log('most likey symptoms *......*', mostLikelySymptoms);
-        symptomResponseArr.push(mostLikelySymptoms);
-      } catch (err) {
-        console.log('error processing query', err);
-        // res.send('Error');
-      }
-    })
-    .then(function() {
-      try {
-        for (var i = 0; i < test.data.age_interaction[age].length; i++) {
-          for (var j = 0; j < mostLikelySymptoms.length; j++) {
-            if (test.data.age_interaction[age][i] !== mostLikelySymptoms[j]) {
               if (
-                !otherPossibleSymptoms.includes(
-                  test.data.age_interaction[age][i]
-                )
+                test.data.age_interaction[age][i] ===
+                test.data.gender_interaction[gender][j]
               ) {
-                otherPossibleSymptoms +=
+                mostLikelySymptoms +=
                   test.data.age_interaction[age][i] + ' || ';
               }
             }
           }
+          console.log('most likey symptoms *......*', mostLikelySymptoms);
+          symptomResponseArr.push(mostLikelySymptoms);
+        } catch (err) {
+          console.log('error processing query', err);
+          //!!! ALL EMPTY OBJECTS WILL FAIL HERE !!!////
+          resp.json('Error');
         }
-        for (var i = 0; i < test.data.gender_interaction[gender].length; i++) {
-          for (var j = 0; j < mostLikelySymptoms.length; j++) {
-            if (
-              test.data.gender_interaction[gender][i] !== mostLikelySymptoms[j]
-            ) {
-              if (
-                !otherPossibleSymptoms.includes(
-                  test.data.gender_interaction[gender][i]
-                )
-              ) {
-                otherPossibleSymptoms +=
-                  test.data.gender_interaction[gender][i] + ' || ';
+      })
+      .then(function() {
+        try {
+          for (var i = 0; i < test.data.age_interaction[age].length; i++) {
+            for (var j = 0; j < mostLikelySymptoms.length; j++) {
+              if (test.data.age_interaction[age][i] !== mostLikelySymptoms[j]) {
+                if (
+                  !otherPossibleSymptoms.includes(
+                    test.data.age_interaction[age][i]
+                  )
+                ) {
+                  otherPossibleSymptoms +=
+                    test.data.age_interaction[age][i] + ' || ';
+                }
               }
             }
           }
-        }
-        // console.log('other possible symptoms *......*', otherPossibleSymptoms);
-        symptomResponseArr.push(otherPossibleSymptoms);
-        console.log('symptomResponseArr is returning:', symptomResponseArr);
+          for (
+            var i = 0;
+            i < test.data.gender_interaction[gender].length;
+            i++
+          ) {
+            for (var j = 0; j < mostLikelySymptoms.length; j++) {
+              if (
+                test.data.gender_interaction[gender][i] !==
+                mostLikelySymptoms[j]
+              ) {
+                if (
+                  !otherPossibleSymptoms.includes(
+                    test.data.gender_interaction[gender][i]
+                  )
+                ) {
+                  otherPossibleSymptoms +=
+                    test.data.gender_interaction[gender][i] + ' || ';
+                }
+              }
+            }
+          }
+          // console.log('other possible symptoms *......*', otherPossibleSymptoms);
+          symptomResponseArr.push(otherPossibleSymptoms);
+          console.log('symptomResponseArr is returning:', symptomResponseArr);
 
-        // return data to calling function
-        res.json(symptomResponseArr);
-      } catch (err) {
-        console.log(err);
-        // res.send('Error');
-      }
-    })
-    .catch(function(err) {
-      console.log('there is an error', err);
-      res.json('500');
-    });
-  //} // END Interaction Query Function
+          // return data to calling function
+          res.json(symptomResponseArr);
+        } catch (err) {
+          console.log(err);
+          // res.send('Error');
+        }
+      })
+      .catch(function(err) {
+        console.log('there is an error', err);
+        res.json('500');
+      });
+  } // END Interaction Query Function
 }); // END INTERACTION API
 
 module.exports = router;
